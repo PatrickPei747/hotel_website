88c4c1fa0c3cf234ebfc2f6954d6267f78bf655b
PatrickPei747 <754463323@qq.com>
2023-01-05 17:52:24 +1300

    init

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Header from "./components/Header";
+import Welcome from "./pages/Welcome";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/about" exact>
+            <About />
+          </Route>
+          <Route path="/contact" exact>
+            <Contact />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
